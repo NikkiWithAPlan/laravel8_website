c1b6e62abd630eb69b819252741a1fedb99a0a28
Nikki Szedljak <sznicci@gmail.com>
2021-09-12 12:15:08 +0100

    004 - Added Band members seeder

---
+++ database/seeders/BandsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bands')->insert([
+            [
+                'member_name' => 'Szedlják János',
+                'instrument' => 'Ének ( Bon Scott )'
+            ],
+            [
+                'member_name' => 'Szólógitárost keresünk',
+                'instrument' => 'Szólógitár ( Angus Young )'
+            ],
+            [
+                'member_name' => 'Szűcs Attila',
+                'instrument' => 'Dobok ( Phill Rudd )'
+            ],
+            [
+                'member_name' => 'Basszusgitarost keresunk',
+                'instrument' => 'Basszusgitár ( Cliff Williams )'
+            ],
+            [
+                'member_name' => 'Szép Gábor',
+                'instrument' => 'Ritmusgitár ( Malcolm Young )'
+            ],
+        ]);
+    }
+}
