3e1e605056fd31fd24ede67b3fb1cd56f3ad775f
Nikki Szedljak <nikoletta.szedljak@gmail.com>
2021-09-26 18:18:17 +0100

    051 - Updated HOME route for Register purposes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/band';
 
     /**
      * The controller namespace for the application.
